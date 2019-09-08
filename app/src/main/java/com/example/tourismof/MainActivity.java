4c1db30f3d74d82072b072b3a1f9f8b2531a960c
Uzair <uzairalimurtaza@gmail.com>
2019-09-08 18:53:04 +0500

    Toolbar working + Recycler View of Navigation bar

--- app/src/main/java/com/example/tourismof/MainActivity.java
+++ app/src/main/java/com/example/tourismof/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.tourismof;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,24 +21,65 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private RecyclerView recyclerView;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar mtoolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        DrawerLayout drawerLayout=(DrawerLayout) findViewById(R.id.drawyer_layout);
-//        NavigationView navigationView=(NavigationView) findViewById(R.id.navigation_view);
-//
-//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-////                UserMenuSelector(item);
-//                return false;
-//            }
-//        });
+
+        mtoolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mtoolbar);
+        getSupportActionBar().setTitle("Home");
 
 
+        drawerLayout= findViewById(R.id.drawyer_layout);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.drawyer_open,R.string.drawyer_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView=(NavigationView) findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserMenuSelector(item);
+                return false;
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void UserMenuSelector(MenuItem item)
+    {
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_Profile:
+                Toast.makeText(getApplicationContext(),"Profile",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(getApplicationContext(),"Settings",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(getApplicationContext(),"Logout",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
 
